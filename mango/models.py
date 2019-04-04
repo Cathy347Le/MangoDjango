c65533bd5b7f685b2b2f1d6622e6ec2e7ac9a147
Wesley Scholl <clients@konjoweb.com>
2019-04-04 15:33:32 -0400

    created models and migrated

--- mango/models.py
+++ mango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=750)
+    product_image = models.CharField(max_length=500)
+    price = models.IntegerField()
+
+    def __str__(self):
+            return self.name
+
+
+class Cart(models.Model):
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+            return self.name
