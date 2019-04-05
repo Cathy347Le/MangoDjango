10d048a73910b6f544069964c7e7a1f6f2d486fa
Cathy <Cathy347Le@gmail.com>
2019-04-05 12:53:06 -0400

    added chat_widget

--- mango_project/settings.py
+++ mango_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mango'
+    'mango',
+    'drift_chatwidget'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DRIFT_CHAT_WIDGET = {'ID': 'eyi57hd4ewaf'}
