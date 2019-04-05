710c11bef1b82aa124daedbd901760cc80f9ffa2
Cathy <Cathy347Le@gmail.com>
2019-04-05 11:49:45 -0400

    update views

--- mango/views.py
+++ mango/views.py
@@ -8,25 +8,30 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'mango/product_list.html', {'products': products})
 
+
 def cart_list(request):
     carts = Cart.objects.all()
     return render(request, 'mango/cart_list.html', {'carts': carts})
+
 
 def product_detail(request, pk):
     product = Product.objects.get(id=pk)
     return render(request, 'mango/product_detail.html', {'product': product})
 
+
 def cart_detail(request, pk):
     cart = Cart.objects.get(id=pk)
     return render(request, 'mango/cart_detail.html', {'cart': cart})
 
+
 def addto_cart(request, pk):
     product = Product.objects.get(id=pk)
     product.cart_id = 1
-    product.quantity = F('quantity') +1
+    product.quantity = F('quantity') + 1
     product.save()
     cart = Cart.objects.get(id=1)
     return render(request, 'mango/cart_detail.html', {'cart': cart})
+
 
 def deletefrom_cart(request, pk):
     product = Product.objects.get(id=pk)
